4e3c692830a1c1e28693824a8c6acf38e6b2cc83
Gusain, Nidhi <NidhiGusain@quickenloans.com>
2021-09-22 15:46:01 -0400

    added name feature

--- GitHubDemo/Program.cs
+++ GitHubDemo/Program.cs
@@ -6,7 +6,8 @@ namespace GitHubDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello! What is your name: ");
+            string input = Console.ReadLine();
         }
     }
 }
